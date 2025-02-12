c87c8867044b2f7407f005b97e2d31942cec2adf
tjbanuelos <tomasbanuelos@Tomass-MacBook-Pro-5.local>
2025-02-12 17:17:48 -0600

    Full data and analysis of net imports for review

---
+++ api_pulls.py
@@ -0,0 +1,5 @@
+# The Nasdaq api request for energy imports worldbank data from the top 10 countries by GDP (2024)
+"https://data.nasdaq.com/api/v3/datatables/WB/DATA.csv?series_id=EG.IMP.CONS.ZS&country_code=USA%2CJPN%2CGBR%2CFRA%2CRUS%2CCAN%2CITA%2CIND%2CCHN%2CDEU&api_key={YOUR API KEY HERE}"
+
+# The Nasdaq api request for gdp worldbank data from the top 10 countries by GDP (2024)
+"https://data.nasdaq.com/api/v3/datatables/WB/DATA.csv?series_id=NY.GDP.MKTP.CD&country_code=USA%2CJPN%2CGBR%2CFRA%2CRUS%2CCAN%2CITA%2CIND%2CCHN%2CDEU&api_key={YOUR API KEY HERE}"
